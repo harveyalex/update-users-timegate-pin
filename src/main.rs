bae563322305f12c667e2295b6102a7381ff382f
Alex Harvey <alex.harvey@workwave.com>
2024-06-13 15:54:32 +1000

    all the threads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use anyhow::Result;
 use dotenv::dotenv;
-use futures::stream::FuturesUnordered;
-use futures::stream::StreamExt;
 use mongodb::{
     bson::{doc, oid::ObjectId, Document},
     Client,
@@ -12,7 +10,6 @@ use std::str::FromStr;
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv()?;
-    let time = std::time::Instant::now();
     let application_id = ObjectId::from_str(
         std::env::args()
             .nth(1)
@@ -34,12 +31,9 @@ async fn main() -> Result<()> {
     let client = Client::with_uri_str(&mongo_uri).await?;
     let db = client.database(database_id);
 
-    let mut futures = FuturesUnordered::new();
-    let mut outputs = Vec::new();
-
     csv_lines.remove(0); // ignore the headers
     let mut would_have_been_updated: Vec<ApplicationUser> = vec![];
-
+    let mut handles = vec![];
     for line in csv_lines {
         let mut user = line.split(",");
         let (first_name, last_name, pin) = match (user.next(), user.next(), user.nth(5)) {
@@ -67,7 +61,19 @@ async fn main() -> Result<()> {
 
             continue;
         }
+        let db = db.clone();
+        let filter = filter.clone();
+        let update = update.clone();
+        handles.push(tokio::spawn(async move {
+            let db_result = db
+                .collection::<Document>("applicationusers")
+                .update_one(filter, update, None)
+                .await
+                .expect("Should not error updating user");
+            db_result
+        }));
     }
+
     if is_dry_run == "true" {
         println!("Would have updated: {:?}", would_have_been_updated.len());
         for user in would_have_been_updated {
@@ -84,7 +90,18 @@ async fn main() -> Result<()> {
         }
         return Ok(());
     }
+    for handle in handles {
+        match handle.await {
+            Ok(result) => {
+                println!("Modified: {:?}", result.modified_count);
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
+        }
+    }
     Ok(())
+    // use futures::stream::StreamExt;
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -110,84 +127,84 @@ struct Options {
     employeePIN: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let now = std::time::Instant::now();
-    dotenv()?;
+// #[tokio::main]
+// async fn main() -> Result<()> {
+//     let now = std::time::Instant::now();
+//     dotenv()?;
 
-    let application_id = ObjectId::from_str(
-        std::env::args()
-            .nth(1)
-            .unwrap_or("000000000000000000000000".to_string())
-            .as_str(),
-    )?;
-    let database_id = std::env::args().nth(2).unwrap_or("development".to_string());
-    let database_id = &database_id.as_str();
-    let is_dry_run = std::env::var("DRY_RUN").is_ok_and(|x| x == "true".to_string());
+//     let application_id = ObjectId::from_str(
+//         std::env::args()
+//             .nth(1)
+//             .unwrap_or("000000000000000000000000".to_string())
+//             .as_str(),
+//     )?;
+//     let database_id = std::env::args().nth(2).unwrap_or("development".to_string());
+//     let database_id = &database_id.as_str();
+//     let is_dry_run = std::env::var("DRY_RUN").is_ok_and(|x| x == "true".to_string());
 
-    let csv_file = std::env::var("CSV_FILE")?;
+//     let csv_file = std::env::var("CSV_FILE")?;
 
-    println!("Application ID: {:?}", application_id);
-    println!("Is dry run: {:?}", is_dry_run);
+//     println!("Application ID: {:?}", application_id);
+//     println!("Is dry run: {:?}", is_dry_run);
 
-    let csv_data = std::fs::read_to_string(&csv_file)?;
-    let mut csv_lines: Vec<String> = csv_data.lines().map(|line| line.to_string()).collect();
+//     let csv_data = std::fs::read_to_string(&csv_file)?;
+//     let mut csv_lines: Vec<String> = csv_data.lines().map(|line| line.to_string()).collect();
 
-    let mongo_uri = std::env::var("MONGO_URI")?;
-    let client = Client::with_uri_str(&mongo_uri).await?;
-    let db = client.database(database_id);
-    let mut would_have_been_updated: Vec<ApplicationUser> = vec![];
-    csv_lines.remove(0); // ignore the headers
+//     let mongo_uri = std::env::var("MONGO_URI")?;
+//     let client = Client::with_uri_str(&mongo_uri).await?;
+//     let db = client.database(database_id);
+//     let mut would_have_been_updated: Vec<ApplicationUser> = vec![];
+//     csv_lines.remove(0); // ignore the headers
 
-    let mut handles = vec![];
-    for line in csv_lines {
-        let mut user = line.split(",");
-        let (first_name, last_name, pin) = match (user.next(), user.next(), user.nth(5)) {
-            (Some(first_name), Some(last_name), Some(pin)) => (first_name, last_name, pin),
-            _ => continue,
-        };
+//     let mut handles = vec![];
+//     for line in csv_lines {
+//         let mut user = line.split(",");
+//         let (first_name, last_name, pin) = match (user.next(), user.next(), user.nth(5)) {
+//             (Some(first_name), Some(last_name), Some(pin)) => (first_name, last_name, pin),
+//             _ => continue,
+//         };
 
-        let filter = doc! { "application": application_id.clone(), "firstName": first_name, "lastName": last_name, "deleted": false};
-        let update = doc! { "$set": { "plugins.timegate.options.EmployeePIN": pin } };
+//         let filter = doc! { "application": application_id.clone(), "firstName": first_name, "lastName": last_name, "deleted": false};
+//         let update = doc! { "$set": { "plugins.timegate.options.EmployeePIN": pin } };
 
-        if is_dry_run {
-            would_have_been_updated.push(
-                db.collection::<ApplicationUser>("applicationusers")
-                    .find_one(filter.clone(), None)
-                    .await?
-                    .expect("User should be found"),
-            );
+//         if is_dry_run {
+//             would_have_been_updated.push(
+//                 db.collection::<ApplicationUser>("applicationusers")
+//                     .find_one(filter.clone(), None)
+//                     .await?
+//                     .expect("User should be found"),
+//             );
 
-            continue;
-        }
+//             continue;
+//         }
 
-        let new_db = db.clone();
-        handles.push(tokio::spawn(async move {
-            let db_result = new_db
-                .collection::<Document>("applicationusers")
-                .update_one(filter, update, None)
-                .await
-                .expect("Should not error updating user");
-            db_result
-        }));
-    }
+//         let new_db = db.clone();
+//         handles.push(tokio::spawn(async move {
+//             let db_result = new_db
+//                 .collection::<Document>("applicationusers")
+//                 .update_one(filter, update, None)
+//                 .await
+//                 .expect("Should not error updating user");
+//             db_result
+//         }));
+//     }
 
-    if is_dry_run {
-        return Ok(());
-    }
+//     if is_dry_run {
+//         return Ok(());
+//     }
 
-    let results = futures::future::join_all(handles).await;
-    for result in results {
-        match result {
-            Ok(result) => {
-                println!("Modified: {:?}", result.modified_count);
-            }
-            Err(e) => {
-                println!("Error: {:?}", e);
-            }
-        }
-    }
-    let now = now.elapsed();
-    println!("Time elapsed: {:?}", now);
-    Ok(())
-}
+//     let results = futures::future::join_all(handles).await;
+//     for result in results {
+//         match result {
+//             Ok(result) => {
+//                 println!("Modified: {:?}", result.modified_count);
+//             }
+//             Err(e) => {
+//                 println!("Error: {:?}", e);
+//             }
+//         }
+//     }
+//     let now = now.elapsed();
+//     println!("Time elapsed: {:?}", now);
+//     Ok(())
+// }
